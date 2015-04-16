b4357035a17c257c2a4b2c89b0da6a96ae911ea0
Isaac Ezer <isaac.ezer@gmail.com>
2015-04-16 15:32:44 -0400

    fixes

--- app/components/ie-test.js
+++ app/components/ie-test.js
@@ -1,3 +1,6 @@
 import ieTest from 'isaac-config/components/ie-test';
+import config from '../config/environment';
 
-export default ieTest;
+export default ieTest.extend({
+  name: config.name
+});
